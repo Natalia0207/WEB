2e0a079a59d84ec08e40207681014b07e3a045b9
Natalia0207 <110469410+Natalia0207@users.noreply.github.com>
2023-08-03 19:55:03 -0500

    Add files via upload

---
+++ challenge 1 - natalia Hernadez Motta.js
@@ -0,0 +1,22 @@
+// flecha
+const flecha =(number) => {
+    if (number % 2 === 0) {
+        console.log(number + " is even.");
+      } else {
+        console.log(number + " is odd.");
+      }
+    };
+
+number(5);
+
+//Regular
+
+function flecha (number) {
+    if (number % 2 === 0) {
+        console.log(number + " is even.");
+      } else {
+        console.log(number + " is odd.");
+      }
+    };
+
+number(4);
