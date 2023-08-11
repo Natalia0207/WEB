486451e46e7cd2c000d760c3e07d952152727733
Natalia0207 <110469410+Natalia0207@users.noreply.github.com>
2023-08-10 21:28:01 -0500

    Add files via upload

---
+++ ejercicio en clase-  semana 3.js
@@ -0,0 +1,67 @@
+const getPromesa = (numberA, numberB) => {
+    return new Promise((resolve, reject) => {
+        if ((numberA - numberB) >= 1) {
+            resolve(numberA - numberB);
+        } else { reject("ERROR"); }
+    })
+}
+
+getPromesa(5, 1)
+.then(data => {
+    console.log(data)
+})
+.catch(error => {
+        console.error(error)
+    });
+
+
+
+    const peticion =async(numberA, numberB)=>{
+        try{
+        int numberA;
+        int numberB,
+        const resp = await (numberA - numberB) >= 1));
+        const data = await ();
+        
+        
+        
+        
+        ((numberA - numberB) >= 1) {
+                    resolve(numberA - numberB);
+                } else { reject("ERROR"); }
+        console.log(data;)
+        
+        }catch (error){
+        
+        
+        
+        
+        
+        //Solución profe:
+        const f2= async() =>{
+        const data = await fn(3,4);
+        console.log(data);
+        }catch(error){
+        consol.logconsole.log(error);
+        }}
+        f2();
+        
+        
+        
+        
+        
+        const getPromesa = (numberA, numberB) => {
+            return new Promise((resolve, reject) => {
+                if ((numberA - numberB) >= 1) {
+                    resolve(numberA - numberB);
+                } else { reject("ERROR"); }
+            })
+        }
+        
+        getPromesa(2, 1)
+        .then(data => {
+            console.log(data)
+        })
+        .catch(error => {
+                console.error(error)
+            });
