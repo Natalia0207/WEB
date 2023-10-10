c96904b15b19844bfa2faae9e735511653f456c9
Natalia0207 <110469410+Natalia0207@users.noreply.github.com>
2023-10-10 17:18:04 -0500

    Add files via upload

---
+++ Challenge 2 (diapo2)/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FirstApp from './FirstApp';
+function App() {
+  return (
+    <div className="App">
+      <FirstApp />
+    </div>
+  )
+}
+export default App; 
---
+++ Challenge 2 (diapo2)/FirstApp.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+const FirstApp = () => {
+  return (
+    <div>
+      <h1>tittle</h1>
+      <span>10</span> 
+    </div>    )
+}
+export default FirstApp;
