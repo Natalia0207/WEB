605e86526d0f797a92afc066df895eb07a8e4cb7
Natalia0207 <110469410+Natalia0207@users.noreply.github.com>
2023-10-10 22:45:30 -0500

    Add files via upload

---
+++ Challenge9 (diapo 5)/src/App.jsx
@@ -0,0 +1,7 @@
+import { TodoApp } from "./components/TodoApp";
+import "./styles.css";
+export default function App() {
+  return ( 
+  <div className="App"> 
+  <TodoApp /> </div> )
+}
---
+++ Challenge9 (diapo 5)/src/components/TodoReducer.jsx
@@ -0,0 +1,7 @@
+export const TodoReducer = (initialState = [], action) => {
+    switch (action.type) {
+        case "[TODO] ADD TODO":
+        return [...initialState, action.payload];
+        default:
+        return initialState; };
+  }
